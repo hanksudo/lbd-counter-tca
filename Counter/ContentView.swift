4a511cfd9d8146c715e8a05a967f305eb2f39eed
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-08-29 21:11:09 +0900

    init commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Hank Wang on 2024/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Hank Wang on 2024/08/29.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
