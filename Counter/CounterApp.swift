7ee0bdcec4e9bf568b3ef46dda9d1b071b877d9d
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-08-29 23:14:09 +0900

    integrate

--- Counter/CounterApp.swift
+++ Counter/CounterApp.swift
@@ -5,13 +5,19 @@
 //  Created by Hank Wang on 2024/08/29.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct CounterApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: CounterApp.store)
         }
     }
 }
--- Counter/CounterView.swift
+++ Counter/CounterView.swift
@@ -13,22 +13,26 @@ struct CounterView: View {
 
     var body: some View {
         VStack {
-            Text("0")
+            Text("\(store.count)")
                 .font(.largeTitle)
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             HStack {
-                Button("-") {}
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.1))
-                    .cornerRadius(10)
-                Button("+") {}
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.1))
-                    .cornerRadius(10)
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
             }
         }
     }
