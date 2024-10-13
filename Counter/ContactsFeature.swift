14ab08b391130673c58b22d6c61dfa52672437e4
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-10-13 16:38:30 +0900

    presentation

--- Counter/ContactsFeature.swift
+++ Counter/ContactsFeature.swift
@@ -35,15 +35,11 @@ struct ContactsFeature {
                     contact: Contact(id: UUID(), name: "")
                 )
                 return .none
-            case .addContact(.presented(.cancelButtonTapped)):
-                state.addContact = nil
-                return .none
-            case .addContact(.presented(.saveButtonTapped)):
-                guard let contact = state.addContact?.contact
-                else { return .none }
+                
+            case let .addContact(.presented(.delegate(.saveContact(contact)))):
                 state.contacts.append(contact)
-                state.addContact = nil
                 return .none
+
             case .addContact:
                 return .none
             }
