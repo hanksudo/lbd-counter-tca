9fa8012688771e32f91047ae09b7a909ba6ae1a8
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-10-13 18:03:10 +0900

    refactor: by destination model

--- Counter/ContactsFeature.swift
+++ Counter/ContactsFeature.swift
@@ -18,37 +18,68 @@ struct Contact: Equatable, Identifiable {
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
+        @Presents var destination: Destination.State?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
 
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
+        case destination(PresentationAction<Destination.Action>)
+        case deleteButtonTapped(id: Contact.ID)
+
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
     }
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
-                
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
                 return .none
 
-            case .addContact:
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
+                state.contacts.remove(id: id)
+                return .none
+
+            case .destination:
+                return .none
+
+            case let .deleteButtonTapped(id: id):
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
+                    }
+                )
                 return .none
             }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
+        .ifLet(\.$destination, action: \.destination)
     }
 }
+
+extension ContactsFeature {
+    @Reducer
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
+    }
+}
+
+extension ContactsFeature.Destination.State: Equatable {}
 
 struct ContactsView: View {
     @Bindable var store: StoreOf<ContactsFeature>
@@ -57,7 +88,16 @@ struct ContactsView: View {
         NavigationStack {
             List {
                 ForEach(store.contacts) { contact in
-                    Text(contact.name)
+                    HStack {
+                        Text(contact.name)
+                        Spacer()
+                        Button {
+                            store.send(.deleteButtonTapped(id: contact.id))
+                        } label: {
+                            Image(systemName: "trash")
+                                .foregroundStyle(.red)
+                        }
+                    }
                 }
             }
             .navigationTitle("Contacts")
@@ -72,12 +112,13 @@ struct ContactsView: View {
             }
         }
         .sheet(
-            item: $store.scope(state: \.addContact, action: \.addContact)
+            item: $store.scope(state: \.destination?.addContact, action: \.destination?.addContact)
         ) { addContactStore in
             NavigationStack {
                 AddContactView(store: addContactStore)
             }
         }
+        .alert($store.scope(state: \.destination.alert, action: \.destination.alert))
     }
 }
 
